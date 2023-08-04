2a53804558dc745bf99e8ac7b9e6bc927ec08012
Wesley Alves Barreto <wesley.alvesxll@gmail.com>
2023-08-04 19:01:32 -0300

    starting and starting classes

---
+++ server.js
@@ -0,0 +1,5 @@
+import App from "./src/app";
+
+App.listen(8000, () => {
+  console.log("Listening on port 8000");
+});
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import homeRouter from "./routes/home";
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use("/", homeRouter);
+  }
+}
+
+export default new App().app;
